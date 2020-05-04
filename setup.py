5945a67d80bb56c39e163ef98488236cf5e702b8
AlaBouali <trap.leader.123@gmail.com>
2020-05-04 22:36:38 +0200

    Add files via upload

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pymysql2",
-    version="1.0.4",
+    version="1.0.5",
     author="AlaBouali",
     author_email="trap.leader.123@gmail.com",
     description="Simple and secure module to perform safe MySQL queries",
